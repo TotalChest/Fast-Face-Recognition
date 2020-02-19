1a33609f12c3a869534cd2616fa3f4296b63326f
TotalChest <13sergey78@mail.ru>
2020-02-19 17:01:03 +0300

    test faiss

--- faiss_search.py
+++ faiss_search.py
@@ -1,23 +1,33 @@
 import numpy as np
 d = 128                        # dimension
-nb = 10000000                    # database size
-nq = 10                      # nb of queries
+nb = 1000000                  # database size
+nq = 1                      # nb of queries
 np.random.seed(1234)             # make reproducible
 xb = np.random.random((nb, d)).astype('float32')
-xb[:, 0] += np.arange(nb) / 1000.
-xq = np.random.random((nq, d)).astype('float32')
-xq[:, 0] += np.arange(nq) / 1000.
+#xb[:, 0] += np.arange(nb) / 1000.
+#xq = np.random.random((nq, d)).astype('float32')*100
+xq = np.zeros((1,d)).astype('float32')
+xq[0,127]=1
+xq += xb[:1,:]
+#xq[:, 0] += np.arange(nq) / 1000.
 
-print('START LEARNING')
+print('LEARNING...')
 
 import faiss                   # make faiss available
 index = faiss.IndexFlatL2(d)   # build the index
 print(index.is_trained)      
+print('ADDING...')
 index.add(xb)           # add vectors to the index
 print(index.ntotal)
 
 
+print('SEARCHING...')
 k = 10                         # we want to see 4 nearest neighbors
-D, I = index.search(xb[:5], k)     # actual search
+#D, I = index.search(xb[:1], k)
+D, I = index.search(xq, k)      # actual search
 print(I)   
 print(D)                # neighbors of the 5 first queries 
+print('VECTORS:')
+print(xq[0,:])
+print(xb[I[0,0],:])
+
